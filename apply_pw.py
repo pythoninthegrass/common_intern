ff8f2c3d9e41dad09317be28ec3c14f868f76ef1
pythoninthegrass <4097471+pythoninthegrass@users.noreply.github.com>
2023-06-19 22:49:16 -0500

    async -> sync

--- apply_pw.py
+++ apply_pw.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import asyncio
+# import asyncio
 import json
 import re
 import requests
@@ -13,7 +13,8 @@ from decouple import config, UndefinedValueError
 from icecream import ic
 # import typer
 from pathlib import Path
-from playwright.async_api import async_playwright, TimeoutError
+from playwright.sync_api import sync_playwright, TimeoutError
+# from playwright.async_api import async_playwright, TimeoutError
 
 # TODO: typer
 
@@ -67,7 +68,7 @@ headers = {
 }
 
 
-async def get_raw_html(url):
+def get_raw_html(url):
     """Get raw HTML from a given URL"""
 
     with suppress(requests.exceptions.ConnectionError):
@@ -76,7 +77,7 @@ async def get_raw_html(url):
             return response.text
 
 
-async def job_prefs():
+def job_prefs():
     """Prompt for job preferences if not set in .env file"""
 
     global position_title, location
@@ -101,7 +102,7 @@ class Driver:
         self.kwargs = kwargs
 
 
-    async def run(self):
+    def run(self):
         headers = {
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'GET',
@@ -130,56 +131,56 @@ class Driver:
             "slow_mo": 100,
             "devtools": False,
         }
-        browser = await self.playwright.chromium.launch(**options)
+        browser = self.playwright.chromium.launch(**options)
         state = Path("playwright/.auth/state.json")
         if state.is_file():
-            context = await browser.new_context(
+            context = browser.new_context(
                 viewport={"width":1280,"height":1400},
                 storage_state=state._str
             )
         else:
-            context = await browser.new_context(
+            context = browser.new_context(
                 viewport={"width":1280,"height":1400}
             )
-        page = await context.new_page()
+        page = context.new_page()
 
         page.set_default_timeout(timeout)
 
-        await page.set_extra_http_headers(headers)
+        page.set_extra_http_headers(headers)
 
-        await page.goto(self.url)
+        page.goto(self.url)
 
         # save state (cookies, local storage, etc.)
-        await context.storage_state(path=state._str)
+        context.storage_state(path=state._str)
 
         # run class methods
         if self.kwargs:
             if self.kwargs['action'] == 'login':
-                await self.login(page)
+                self.login(page)
             elif self.kwargs['action'] == 'easy_apply':
-                await self.easy_apply(page, self.url)
+                self.easy_apply(page, self.url)
             elif self.kwargs['action'] == 'greenhouse':
-                await self.greenhouse(page, self.url)
+                self.greenhouse(page, self.url)
             elif self.kwargs['action'] == 'lever':
-                await self.lever(page, self.url)
+                self.lever(page, self.url)
 
         # ! pause for debugging
-        await page.pause()
+        page.pause()
 
         # close browser
-        await page.close()
-        await context.close()
-        await browser.close()
+        page.close()
+        context.close()
+        browser.close()
 
 
-    async def login(self, page):
+    def login(self, page):
         """Helper method to give user time to log into Glassdoor"""
 
         print('Waiting for user to log in...')
 
         while True:
             try:
-                await page.wait_for_selector("body.main.loggedIn.lang-en.en-US.gdGrid")
+                page.wait_for_selector("body.main.loggedIn.lang-en.en-US.gdGrid")
                 timestamp = time.strftime("%H:%M:%S", time.localtime())
                 print(f"Logged in at {timestamp}")
             except TimeoutError:
@@ -187,31 +188,31 @@ class Driver:
             return True
 
 
-    async def easy_apply(self, page, url):
+    def easy_apply(self, page, url):
         """
         Apply to a job listing on Glassdoor directly from the job listing page
         """
 
         # navigate to the application page
-        await page.goto(url)
+        page.goto(url)
 
-        async with page.expect_popup() as new_page_info:
-            await page.get_by_role("button", name="Easy Apply").click()
-        new_page = await new_page_info.value
-        await new_page.wait_for_load_state()
+        with page.expect_popup() as new_page_info:
+            page.get_by_role("button", name="Easy Apply").click()
+        new_page = new_page_info.value
+        new_page.wait_for_load_state()
 
         # assign new page url to variable
-        ez_url = new_page.url
+        # ez_url = new_page.url
 
         # get page html
-        html = await new_page.content()
+        # html = new_page.content()
 
         # # parse html
         # soup = BeautifulSoup(html, 'html.parser')
         # print(soup.prettify())
         # element = soup.find_all("resumeUploadCard-button")
 
-        # elems = await new_page.locator("resumeUploadCard-button").evaluate_all()
+        # elems = new_page.locator("resumeUploadCard-button").evaluate_all()
 
         # dictionary of form fields
         form_fields = {
@@ -238,40 +239,40 @@ class Driver:
                 # match statement
                 match field:
                     case "input-firstName":
-                        await new_page.get_by_test_id(field).fill(value)
+                        new_page.get_by_test_id(field).fill(value)
                     case "input-lastName":
-                        await new_page.get_by_test_id(field).fill(value)
+                        new_page.get_by_test_id(field).fill(value)
                     case "Phone number":
-                        await new_page.get_by_label(field, exact=True).fill(value)
+                        new_page.get_by_label(field, exact=True).fill(value)
                     case "input-email":
-                        await new_page.get_by_test_id(field).fill(value)
+                        new_page.get_by_test_id(field).fill(value)
                     case "resumeUploadCard":
                         # TODO: QA
-                        async with new_page.expect_file_chooser() as file_chooser_info:
-                            await new_page.get_by_test_id(field).click()
-                        file_chooser = await file_chooser_info.value
-                        await file_chooser.set_files(value)
-                        await new_page.get_by_role("button", name="Continue").click()
+                        with new_page.expect_file_chooser() as file_chooser_info:
+                            new_page.get_by_test_id(field).click()
+                        file_chooser = file_chooser_info.value
+                        file_chooser.set_files(value)
+                        new_page.get_by_role("button", name="Continue").click()
                     case "input-q_.*":
-                        await new_page.get_by_test_id(field).fill(value)
+                        new_page.get_by_test_id(field).fill(value)
                     case "Country":
-                        await new_page.get_by_role("combobox", name=field).select_option(value)
+                        new_page.get_by_role("combobox", name=field).select_option(value)
                     case "United States Citizen":
-                        # await new_page.get_by_text(re.compile(r"United States Citizen|US"))
-                        await new_page.locator("label").filter(has_text=r"United States Citizen|US").locator("span").first.click(timeout=5000)
-                        # await new_page.get_by_test_id(re.compile(r"input-q_.*")).get_by_text("YES").click()
+                        # new_page.get_by_text(re.compile(r"United States Citizen|US"))
+                        new_page.locator("label").filter(has_text=r"United States Citizen|US").locator("span").first.click(timeout=5000)
+                        # new_page.get_by_test_id(re.compile(r"input-q_.*")).get_by_text("YES").click()
                     case "I don't wish to answer":
-                        await new_page.get_by_text(re.compile(r"I don't wish to answer")).click()
+                        new_page.get_by_text(re.compile(r"I don't wish to answer")).click()
                     case "Don't recommend me for any jobs at other employers.":
-                        await new_page.get_by_text(re.compile(r"Don't recommend me for any jobs at other employers.")).click()
+                        new_page.get_by_text(re.compile(r"Don't recommend me for any jobs at other employers.")).click()
                     case "Continue":
-                        await new_page.get_by_role("button", name=field).click(timeout=5000)
+                        new_page.get_by_role("button", name=field).click(timeout=5000)
                     case "Review your application":
-                        await new_page.get_by_role("button", name=field).click(timeout=5000)
-                        # await page.get_by_role("button", name="Submit application").click()
+                        new_page.get_by_role("button", name=field).click(timeout=5000)
+                        # page.get_by_role("button", name="Submit application").click()
                     case _:
                         # ! review application
-                        await new_page.pause()
+                        new_page.pause()
                         # pass
 
                 # # OG
@@ -292,7 +293,7 @@ class Driver:
                 # await page.get_by_role("button", name="Submit application").click()
 
 
-    async def greenhouse(self, page, url):
+    def greenhouse(self, page, url):
         """
         Fill out a Greenhouse application form
 
@@ -301,20 +302,20 @@ class Driver:
         """
 
         # navigate to the application page
-        await page.goto(url)
+        page.goto(url)
 
         # basic info
-        await page.frame_locator("iframe[title=\"Greenhouse Job Board\"]").get_by_label("First Name *").fill(first_name)
-        await page.frame_locator("iframe[title=\"Greenhouse Job Board\"]").get_by_label("Last Name *").fill(last_name)
-        await page.frame_locator("iframe[title=\"Greenhouse Job Board\"]").get_by_label("Email *").fill(email)
-        await page.frame_locator("iframe[title=\"Greenhouse Job Board\"]").get_by_label("Phone *").fill(phone)
+        page.frame_locator("iframe[title=\"Greenhouse Job Board\"]").get_by_label("First Name *").fill(first_name)
+        page.frame_locator("iframe[title=\"Greenhouse Job Board\"]").get_by_label("Last Name *").fill(last_name)
+        page.frame_locator("iframe[title=\"Greenhouse Job Board\"]").get_by_label("Email *").fill(email)
+        page.frame_locator("iframe[title=\"Greenhouse Job Board\"]").get_by_label("Phone *").fill(phone)
 
         # Upload Resume
-        await page.frame_locator("iframe[title=\"Greenhouse Job Board\"]").get_by_role("group", name="Resume/CV").get_by_role("button", name="Attach,").click()
-        await page.frame_locator("iframe[title=\"Greenhouse Job Board\"]").get_by_role("group", name="Resume/CV").get_by_role("button", name="Attach,").set_input_files(resume)
+        page.frame_locator("iframe[title=\"Greenhouse Job Board\"]").get_by_role("group", name="Resume/CV").get_by_role("button", name="Attach,").click()
+        page.frame_locator("iframe[title=\"Greenhouse Job Board\"]").get_by_role("group", name="Resume/CV").get_by_role("button", name="Attach,").set_input_files(resume)
 
         # add linkedin
-        await page.frame_locator("iframe[title=\"Greenhouse Job Board\"]").get_by_label("LinkedIn Profile").fill(linkedin)
+        page.frame_locator("iframe[title=\"Greenhouse Job Board\"]").get_by_label("LinkedIn Profile").fill(linkedin)
 
         # TODO: maybeee
         # * add graduation year
@@ -325,14 +326,14 @@ class Driver:
         # * add work authorization
 
         # ! pause for debugging
-        await page.pause()
+        page.pause()
 
         # TODO: enable
         # submit
-        # await page.frame_locator("iframe[title=\"Greenhouse Job Board\"]").get_by_role("button", name="Submit Application").click()
+        # page.frame_locator("iframe[title=\"Greenhouse Job Board\"]").get_by_role("button", name="Submit Application").click()
 
 
-    async def lever(self, page, url):
+    def lever(self, page, url):
         """Fill out a Lever application form"""
 
         pass
@@ -354,19 +355,24 @@ class Driver:
         # driver.find_element_by_class_name('template-btn-submit').click()
 
 
-async def main(url, *args, **kwargs):
-    position_title, location = await job_prefs()
+def main(url, *args, **kwargs):
+    position_title, location = job_prefs()
 
-    async with async_playwright() as playwright:
+    # async with async_playwright() as playwright:
+    #     driver = Driver(playwright, url, *args, **kwargs)
+    #     await driver.run()
+
+    with sync_playwright() as playwright:
         driver = Driver(playwright, url, *args, **kwargs)
-        await driver.run()
+        driver.run()
 
 
 if __name__ == "__main__":
     url = "https://www.glassdoor.com/job-listing/devops-engineer-avive-JV_IC1147334_KO0,15_KE16,21.htm?jl=1008587573785&pos=108&ao=1136043&s=58&guid=0000018897b30beba949200790012d9c&src=GD_JOB_AD&t=SR&vt=w&uido=EFD96E67CACCEAC9A7770DF34F9A3B58&ea=1&cs=1_4c522004&cb=1686172273881&jobListingId=1008587573785&jrtk=3-0-1h2br630km6pt801-1h2br63192ci5001-8eefae5135a7da85-&ctt=1686175543650"
 
     try:
-        asyncio.run(main(url, action='easy_apply'))
+        # asyncio.run(main(url, action='easy_apply'))
+        main(url, action='easy_apply')
     except TimeoutError as e:
         print(e)
         pass
